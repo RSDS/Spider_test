7dad85b8d66e177b6c147ad956114048cb460a45
徐少斌 <xushaobin@kyee.com.cn>
2016-11-16 21:42:22 +0800

    调整方法结构

--- src/SpiderMain.java
+++ src/SpiderMain.java
@@ -14,12 +14,7 @@ import java.util.regex.Pattern;
 public class SpiderMain {
 
 	public static void main(String[] args) {
-		
-		Pattern questionUrlShortFormat = Pattern.compile("[0-9]{4}");
-		Matcher UrlMatcher = questionUrlShortFormat.matcher("5555");
-		Pattern zhihuUrlFormat = Pattern.compile("question_link.+?href=\"(.+?)\"");
-		Matcher x1 = zhihuUrlFormat.matcher("question_link\" href=\"/question/51783509/answer/128688529\" data-id=\"12931354\"");
-		
+
 		// 定义即将访问的链接
 		String url = "https://www.zhihu.com/explore/recommendations";
 		String urlResult = SendGet(url);
@@ -31,11 +26,13 @@ public class SpiderMain {
 		System.out.println(result);
 
 		// 知乎
-		getZhihu(urlResult);
+		Zhihu.getZhihu(urlResult);
 
 		System.out.println(LocalDateTime.now());
 	}
 
+	
+	
 	// SendGet请求
 	public static String SendGet(String url) {
 		// 定义一个字符串用来存储网页内容
@@ -75,43 +72,9 @@ public class SpiderMain {
 
 	}
 
-	// getZhihuAnwser
-	public static Map<String, String> getZhihuAnwser(Zhihu zhihuParameter) {
-		Map<String, String> resultMap = new HashMap();
-		String htmlStr = SendGet(zhihuParameter.zhihuUrl);
-		System.out.println(htmlStr + "\n");
 
-		return resultMap;
-	}
 
-	// 知乎问题链接统一
-	public static String uniUrl(String url) {
-		Pattern questionUrlShortFormat = Pattern.compile("[0-9]{4}");
-		Matcher UrlMatcher = questionUrlShortFormat.matcher("5555");
-		return UrlMatcher.group(1);
-	}
 
-	// getZhihu
-	public static List<Zhihu> getZhihu(String parameter) {
-		Pattern questionFormat = Pattern.compile("question_link.+?>(.+?)<");
-		Pattern zhihuUrlFormat = Pattern.compile("question_link.+?href=\"(.+?)\"");
 
-		List<Zhihu> resultList = new ArrayList<Zhihu>();
-		Matcher questionMatcher = questionFormat.matcher(parameter);
-		Matcher zhihuUrlMatcher = zhihuUrlFormat.matcher(parameter);
-		while (questionMatcher.find() && zhihuUrlMatcher.find()) {
-			Zhihu zhihuTemp = new Zhihu();
-			zhihuTemp.question = questionMatcher.group(1);
-			zhihuTemp.zhihuUrl = "https://www.zhihu.com/question/" + uniUrl(zhihuUrlMatcher.group(1));
-			getZhihuAnwser(zhihuTemp);
-			resultList.add(zhihuTemp);
-		}
-		for (Zhihu i : resultList) {
-			System.out.println("问题：" + i.question);
-			System.out.println("链接：" + i.zhihuUrl + "\n");
-		}
-
-		return resultList;
-	}
 
 }
--- src/Zhihu.java
+++ src/Zhihu.java
@@ -2,9 +2,58 @@
 
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Zhihu {
+	
+	// 知乎问题链接统一
+	public static String uniUrl(String url) {
+		Pattern questionUrlShortFormat = Pattern.compile("question/([0-9]*)");
+		Matcher UrlMatcher = questionUrlShortFormat.matcher(url);
+		if(UrlMatcher.find()){
+			return UrlMatcher.group(1);
+		}
+		return "";
+	}
+	
+	// 获取某个知乎问题的答案
+	public static Map<String, String> getZhihuAnwser(Zhihu zhihuParameter) {
+		Map<String, String> resultMap = new HashMap();
+		String htmlStr = SpiderMain.SendGet(zhihuParameter.zhihuUrl);
+		System.out.println(htmlStr + "\n");
+
+		return resultMap;
+	}
+
+	// 获取知乎问题答案列表
+	public static List<Zhihu> getZhihu(String parameter) {
+		Pattern questionFormat = Pattern.compile("question_link.+?>(.+?)<");
+		Pattern zhihuUrlFormat = Pattern.compile("question_link.+?href=\"(.+?)\"");
+		List<Zhihu> resultList = new ArrayList<Zhihu>();
+		Matcher questionMatcher = questionFormat.matcher(parameter);
+		Matcher zhihuUrlMatcher = zhihuUrlFormat.matcher(parameter);
+		while (questionMatcher.find() && zhihuUrlMatcher.find()) {
+			Zhihu zhihuTemp = new Zhihu();
+			zhihuTemp.question = questionMatcher.group(1);
+			zhihuTemp.zhihuUrl = "https://www.zhihu.com/question/" + Zhihu.uniUrl(zhihuUrlMatcher.group(1));
+			getZhihuAnwser(zhihuTemp);
+			resultList.add(zhihuTemp);
+		}
+		for (Zhihu i : resultList) {
+			System.out.println("问题：" + i.question);
+			System.out.println("链接：" + i.zhihuUrl + "\n");
+		}
+
+		return resultList;
+	}
+	
+	
+	
+	//类实例
 	public String question;
 	public String zhihuUrl;
 	public String questionDesperation;
@@ -16,8 +65,6 @@ public class Zhihu {
 		this.questionDesperation = "";
 		this.anwserMap = new HashMap<String, String>();
 	}
-	
-	
 	
 	
 	
@@ -36,6 +83,7 @@ public class Zhihu {
 		
 		return result;
 	}
+	
 	
 	@Override
 	public String toString(){
