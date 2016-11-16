56775dfc04a7869bd05e67214f22ec7f7c0ac201
徐少斌 <xushaobin@kyee.com.cn>
2016-11-17 01:09:40 +0800

    IO

---
+++ src/SpiderIO.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author XXX
+ *
+ */
+public class SpiderIO {
+	
+	//创建文件
+	public static boolean createNewFile(String filePath){
+		//将"\\"转为"/"
+		String filePathTurn = filePath.replaceAll("\\\\", "/");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return true;
+	}
+
+}
