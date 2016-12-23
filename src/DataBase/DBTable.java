38dd3b63ddaba75bde44546b10c78696ddbadec2
徐少斌 <xushaobin@kyee.com.cn>
2016-12-23 10:29:15 +0800

    数据库操作

--- src/DataBase/DBTable.java
+++ src/DataBase/DBTable.java
@@ -1,4 +1,4 @@
-package textEditor;
+package DataBase;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
-public class Text {
+public class DBTable {
 
 	
 	public static Connection getConnection(){
